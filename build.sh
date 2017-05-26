4c5eca7acbe254be5fe2ee5ffa01c8e619231cc5
JavAster <minefach@gmail.com>
2017-05-26 08:08:44 +0200

    Make toolchain working

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-export CROSS_COMPILE=/home/javaster/JavKernel/prebuilts/gcc/linux-x86/arm/arm-linux-eabi-UB-4.8/bin/arm-eabi-
+export CROSS_COMPILE=/home/javaster/JavKernel/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-UB-4.9/bin/arm-linux-androideabi-
 make ARCH=arm lineageos_shinano_leo_defconfig
 make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE -j8
