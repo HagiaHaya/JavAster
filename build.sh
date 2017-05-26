1e369238b310ac5f94c827f9e2a4d99d9feccf27
JavAster <minefach@gmail.com>
2017-05-26 08:29:09 +0200

    Make it bootable

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
 export CROSS_COMPILE=/home/javaster/JavKernel/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-UB-4.9/bin/arm-linux-androideabi-
 make ARCH=arm lineageos_shinano_leo_defconfig
 make ARCH=arm CROSS_COMPILE=$CROSS_COMPILE -j8
+sh prebuilts/AIK-Linux/cleanup.sh
+sh prebuilts/AIK-Linux/unpackimg.sh
+rm prebuilts/AIK-Linux/split_img/boot.img-zImage
+mv arch/arm/boot/zImage-dtb prebuilts/AIK-Linux/split_img/boot.img-zImage
+sh prebuilts/AIK-Linux/repackimg.sh --original
